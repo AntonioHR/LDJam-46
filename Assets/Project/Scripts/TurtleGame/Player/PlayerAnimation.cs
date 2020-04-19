a945aa4c3048c74962f927217ab6646aaa709b30
Anderson Santos <andersonsantosbr1@gmail.com>
2020-04-18 22:20:12 -0300

    Add Player Jump animation

--- Assets/Project/Scripts/TurtleGame/Player/PlayerAnimation.cs
+++ Assets/Project/Scripts/TurtleGame/Player/PlayerAnimation.cs
@@ -2,21 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerAnimation
-{
+public class PlayerAnimation {
     [SerializeField]
     private Animator animator;
 
     private const string Key_Speed = "Speed";
+    private const string Key_Jumping = "Jumping";
+    private const float Delta_BlendMoveAnimation = 5f;
 
     private MoveByInput movement;
+    private CharacterController charController;
 
-    public PlayerAnimation (MoveByInput movement, Animator animator) {
+    public PlayerAnimation(MoveByInput movement, CharacterController charController, Animator animator) {
         this.movement = movement;
         this.animator = animator;
+        this.charController = charController;
     }
 
-    public void Update () {
-        animator.SetFloat(Key_Speed, movement.DeltaMovement);
+    public void Update() {
+        animator.SetFloat(Key_Speed, Mathf.MoveTowards(animator.GetFloat(Key_Speed), movement.DeltaMovement, Delta_BlendMoveAnimation * Time.deltaTime));
+    }
+
+    public void OnLeftGround() {
+        //Debug.Log("OnLeftGround");
+        animator.SetBool(Key_Jumping, true);
+    }
+
+    public void OnBecameGrounded() {
+       // Debug.Log("OnBecameGrounded");
+        animator.SetBool(Key_Jumping, false);
     }
 }
